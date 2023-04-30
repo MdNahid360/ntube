8c2b87ac2fb68336c464e2aad5943ed6fc40f85d
md nahid <mdnahid360s@gmail.com>
2023-04-30 23:50:45 +0600

    update

---
+++ src/pages/Component/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <nav className="bg-[#1c213188] py-4 px-20 flex justify-between">
+            <h2 className="font-bold text-xl">NTube</h2>
+            {/* <input type="text" className=" py-1 px-2 bg-[#1d2e4d4f] border border-[#0073ff4e] rounded w-[230px]" placeholder='Search...'/> */}
+        </nav>
+    );
+};
+
+export default Navigation;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,124 +1,72 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import 'plyr/dist/plyr.css';
+import Plyr from 'plyr';
+import { TbPlayerPlayFilled, TbPlayerPauseFilled} from 'react-icons/tb';
+import { useRef, useEffect, useState } from 'react';
+import Navigation from './Component/Navigation';
+import ReactPlayer from 'react-player'
+;
 
-const inter = Inter({ subsets: ['latin'] })
+import dynamic from 'next/dynamic';
+const DynamicReactPlayer = dynamic(() => import('react-player'), {
+  ssr: false,
+});
+
 
 export default function Home() {
+  const [url, setUrl] = useState('https://www.youtube.com/watch?v=b8NxzFyGKtQ&ab_channel=Quran');
+  const [autoPlay, setAutoPlay] = useState(false)
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const player = new Plyr(videoRef.current);
+  }, []);
+
+  const updateUrl =(e)=>{
+    e.preventDefault();
+     const form = e.target;
+     setUrl(form.link.value);
+     form.reset()
+  }
+  const handleFileChange = (event) => {
+  const file = event.target.files[0];
+  const reader = new FileReader();
+
+  reader.onload = (event) => {
+    const dataUrl = event.target.result;
+    setUrl(dataUrl);
+  };
+
+  reader.readAsDataURL(file);
+};
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="">
+      {/* <Navigation /> */}
+       
+        <div className="w-[938px] m-auto mt-5 px-4">
+             <div className="flex items-center justify-between border-b border-[gray] pb-2">
+                  <h3 className="font-bold text-xl">NTube</h3>
+                    <input type="file" onChange={handleFileChange} />
+                    
+                  <form onSubmit={updateUrl} className="flex items-center">
+                       <input type="text" name='link' className="bg-[#0d3b4745] border border-[#12618b] px-3 py-1 w-[270px]" placeholder='video URL...' />  <input type="submit" value="preview" className="px-3 py-1 border border-[#0786c1] cursor-pointer bg-[#0786c1]" />
+                  </form>
+             </div> <br />
+          <div className="relative">
+                   <DynamicReactPlayer 
+                url={url} 
+                playing={autoPlay} 
+                controls={true} 
+                volume={0.5} 
+                  height='480px' 
+                  width='100%' 
+                  className="border-2 rounded-lg overflow-hidden border-[#00a6ff49]"/>
+              
+                <button onClick={()=> setAutoPlay(!autoPlay)} className="bg-[#1073b0] float-right mt-3 rounded flex items-center justify-center px-3 py-2">Autoplay / {autoPlay ? 'ON' : 'OFF'}</button>
+          </div>
+           <br />
+         
+            </div>
     </main>
   )
 }
